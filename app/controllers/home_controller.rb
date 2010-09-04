cb08ee778188cd974393c68b275fb90b60dda508
Alex Costa <alex@deanx.com.br>
2010-09-03 22:55:34 -0300

    Edicao e insercao de conhecimentos

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-	@conhecimentos = Conhecimento.find (:all, :select => "id, titulo, autor_id")
+	@conhecimentos = Conhecimento.find (:all, :select => "id, titulo, autor_id", :order => "id DESC")
 	@autors = Autor.find(:all)
   end
 end
